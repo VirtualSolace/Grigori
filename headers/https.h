8b2515efdc35ba34fd5bdfed693f1e5f535b6a72
VirtualSolace <146604439+VirtualSolace@users.noreply.github.com>
2026-06-26 10:10:39 -0600

    Update https.h

--- headers/https.h
+++ headers/https.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "net.h"
-#include <openssl/ssl.h>
+
+#include <curl/curl.h>
 #include <string>
 #include <vector>
 
@@ -9,27 +10,34 @@ static constexpr int PORT = 2213;
 
 class Https : public Net {
 private:
-    SSL* ssl;
-    SSL_CTX* ctx;
+    CURL* curl;
 
     std::string path;
     std::string host;
     std::string AT;
-    std::string buffer;
+
+    // Stores the full HTTP response body.
+    std::string responseBuffer;
+
+    std::string serverIP;
+
+    static size_t writeCallback(char* ptr, size_t size, size_t nmemb, void* userdata);
+
+    int sendRequest(const std::string& url, const std::string& method, const std::string& body, bool keepAlive);
 
 public:
     Https();
     ~Https() override;
 
-    int httpsInit(const std::string& ip);   // int port
-    int teardown();
+    int httpsInit(const std::string& ip);
 
-    int sendData(const std::string& data);
+    int teardown() override;
 
     int craftGet(int i, int j);
 
-    int craftPost(int j, int i, const std::string& postData = "");
+    int craftPost(int i, int j, const std::string& postData = "");
 
     int readResponse(std::string& response);
+
     std::vector<std::string> recvData();
 };
